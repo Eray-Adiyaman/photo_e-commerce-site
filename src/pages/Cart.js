729e1fc5120e92f86e7f327a0a05753bf3c2912f
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-04-13 13:59:00 +0300

    photo_e-commerce-site

---
+++ src/pages/Cart.js
@@ -0,0 +1,8 @@
+
+function Cart(){
+    <main className="cart-page">
+        <h1>checkout here</h1>
+    </main>
+}
+
+export default Cart
---
+++ src/pages/Photos.js
@@ -0,0 +1,11 @@
+function Photos (){
+return(
+    <main className="photos">
+        <h1>images go here</h1>
+    </main>
+)
+
+}
+
+
+export default Photos

28fcc09a2c88b8f54064a4f08cfd0c94cf1067ba
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-04-17 17:51:18 +0300

    favorite icon,cart icon ,add & remove from Cart

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,43 @@
+import { useContext, useState } from "react"
+import { Context } from "../contexts/Context"
 
 
 function Image({className,img}){
 
+    const [hovered,setHovered] = useState(false)
+    const {toggleFavorite,addToCart,cartItems,removeFromCart} = useContext(Context)
+
+
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+    
+
+    function cartIcon() {
+        const isInCart = cartItems.some(item => item.id === img.id)
+        if(isInCart){
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img)}></i>
+        }else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        }
+    }
+    
+   
+   
+    
     return(
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`}
+            onMouseEnter={()=> setHovered(true)}
+            onMouseLeave={()=> setHovered(false)}
+        >
             <img src={img.url} alt="to shut up ESlint " className="image-grid"/>
+            {heartIcon()}
+            {cartIcon()}
         </div>
     )
 
--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -5,6 +5,8 @@ const Context = createContext()
 function ContextProvider(props){
 
     const [allPhotos,setAllphotos] =useState([])
+    const [cartItems,setCartitems] = useState([])
+
 
     const url ="https://api.jsonbin.io/b/625682fadc6e0124e8bfdc43/2";
 
@@ -16,8 +18,31 @@ function ContextProvider(props){
     },[])
 
 
+    const toggleFavorite =(id)=>{
+        const updatedArray = allPhotos.map(photo => {
+            if(photo.id === id){
+                return {
+                    ...photo,
+                    isFavorite: !photo.isFavorite
+                }
+            }
+            return photo
+        })
+
+        setAllphotos(updatedArray)
+    }
+
+
+    const addToCart=(imgObj)=>{
+        setCartitems(prev => [...prev,imgObj])
+    }
+
+    const removeFromCart=(imgObj)=>{
+        setCartitems(cartItems.filter(item => item !== imgObj))
+    }
+
     return (
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos,cartItems,toggleFavorite,addToCart,removeFromCart}}>
             {props.children}
         </Context.Provider>
     )
